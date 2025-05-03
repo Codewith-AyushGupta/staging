cc4744df8202013152c47d5b1fd14ad0c07bf127
unknown <Codewith-AyushGupta>
2025-05-04 00:02:55 +0530

    improvements

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World V5.</div>;
+  return <div className="App">Hello World V65.</div>;
 }
 
 export default App;
